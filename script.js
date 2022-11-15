6b39d675ee0e1410e7d172c1ba1d7ad32cbdb57a
graziacrp <graziacarpineta25@gmail.com>
2022-11-15 17:23:27 +0100

    ok

---
+++ script.js
@@ -0,0 +1,36 @@
+
+let arraylistaDellaSpesa = ['Nutella', 'pane', 'latte', 'pasta', 'insalata'];
+
+
+
+
+let listaDellaSpesa = document.getElementById('listadellaspesa');
+
+
+//Ciclo for
+for (let i = 0; i < arraylistaDellaSpesa.lenght; i++) {
+    console.log(arraylistaDellaSpesa[i]);
+
+}
+
+
+
+
+/*Ciclo while
+
+
+
+
+let i = 0;
+while (i < arraylistaDellaSpesa.length) {
+    
+
+
+    console.log(arraylistaDellaSpesa[i]);
+
+    listaDellaSpesa.innerHTML+= "<li>" + arraylistaDellaSpesa[i] + "</li>";
+    i++;
+}
+
+*/
+
